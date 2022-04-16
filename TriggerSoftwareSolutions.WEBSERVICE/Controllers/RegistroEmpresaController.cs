be00af01c76d1849a4226193012b14622181f23c
ResurecTionRx <jvc286@outlook.com>
2022-04-16 11:31:43 -0500

    Reforma Web API
    UPDATE Web API

---
+++ TriggerSoftwareSolutions.WEBSERVICE/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TriggerSoftwareSolutions.DATOS;
+using TriggerSoftwareSolutions.ENTITY.Parametros;
+
+namespace TriggerSoftwareSolutions.WEBSERVICE.Controllers
+{
+    [RoutePrefix("api/RegistroEmpresa")]
+    public class RegistroEmpresaController : ApiController
+    {
+        private DAPaises dapises;
+
+        public RegistroEmpresaController()
+        {
+            dapises = new DAPaises();
+        }
+        [HttpPost]
+        [Route("listarPaises")]
+        public IHttpActionResult listarPaises(ENRegistroEmpresa paramss)
+        {
+            try
+            {
+                var rpt = dapises.listarPaises(paramss);
+                return Ok(rpt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
